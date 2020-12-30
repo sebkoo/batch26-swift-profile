a7743d182d14e5342b353fc010db81d825267586
sebkoo <seb.m.koo@gmail.com>
2020-12-30 12:48:49 -0500

    Initial Commit

---
+++ batch26-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  batch26-profile
+//
+//  Created by Sebastian Koo on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
